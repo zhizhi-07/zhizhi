06697b18453de4faef9f15297731407fdbc536c8
zhizhi <2373922440@qq.com>
2025-10-28 08:22:45 +0800

    修复拉黑警告图标HTML结构问题

--- src/pages/ChatDetail.tsx
+++ src/pages/ChatDetail.tsx
@@ -5811,12 +5811,11 @@ ${emojiInstructions}
                  
                  {/* 拉黑警告图标 - 与气泡垂直居中 */}
                 {message.type === 'received' && message.blocked && (
-                  <svg className="w-5 h-5 text-red-500 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
+                  <svg className="w-5 h-5 text-red-500 flex-shrink-0 ml-2" fill="currentColor" viewBox="0 0 20 20">
                     <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clipRule="evenodd"/>
                   </svg>
                 )}
                  </div>
-                 
                    {/* 自己消息：气泡在左，头像在右 */}
                   {message.type === 'sent' && (
                     <div className="flex flex-col items-center ml-2">
